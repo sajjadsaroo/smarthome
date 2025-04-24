ecc23b75843ade32e822ee3f39dbec2ddf559b42
Macsaroo <sajjad.sarookhani@gmail.com>
2025-04-24 19:22:35 +0330

    add rule class for handle rules

--- src/Rule.java
+++ src/Rule.java
@@ -1,2 +1,11 @@
 public class Rule {
+    public final String deviceName;
+    public final String time;      // format "HH:MM"
+    public final boolean action;   // true=on, false=off
+
+    public Rule(String deviceName, String time, boolean action) {
+        this.deviceName = deviceName;
+        this.time = time;
+        this.action = action;
+    }
 }
